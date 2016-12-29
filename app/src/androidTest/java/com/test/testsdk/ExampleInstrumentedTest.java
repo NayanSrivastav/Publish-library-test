7078bcb82fb9db9cb2f260fceb58d0a4cfc7288a
Nayan Srivastava <nayansrivastav@yahoo.in>
2016-12-29 14:54:23 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/test/testsdk/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/test/testsdk/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.test.testsdk", appContext.getPackageName());
+        assertEquals("com.test.testsdk", "");
     }
 }
